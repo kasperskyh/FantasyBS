dcdf4f56da26f495f2ac911db90342f19e8dd435
Piotr1288 <pk122943@stud.ur.edu.pl>
2024-10-19 15:51:26 +0200

    Added Iframes
    Added basic invincibility upon taking damage

--- Assets/Goblin/Scripts/Health.cs
+++ Assets/Goblin/Scripts/Health.cs
@@ -5,24 +5,37 @@ using UnityEngine;
 public class Health : MonoBehaviour
 
 {
+    [Header ("Health")]
     [SerializeField]private double maxHealth = 100;
     public double currentHealth;
     Animator animator;
+
+    [Header ("iframes")]
+    [SerializeField]private float iFrameDuration;
+    [SerializeField]private float iFrameDeltaTime;
+    [SerializeField]private int flashCount;
+    //private SpriteRenderer spriteRend;
+    private bool isInvincible = false;
     void Start()
     {
         currentHealth = maxHealth;
+        iFrameDuration = 2;
+        iFrameDeltaTime = 1;
         animator = GetComponent<Animator>();
+        //spriteRend = GetComponent<SpriteRenderer>();
     }
 
 
     public void takeDamage(double damage)
     {
+        if (isInvincible) return;
         currentHealth -= damage;
         animator.SetTrigger("Hurt");
         if (currentHealth <= 0)
         {
             animator.SetBool("isDead", true);
         }
+        StartCoroutine(Invulnerability());
     }
 
     void heal(int healAmount)
@@ -37,5 +50,19 @@ public class Health : MonoBehaviour
     public void Die()
     {
         Destroy(gameObject);
+    }
+    
+    private IEnumerator Invulnerability()
+    {
+    Debug.Log("Player turned invincible!");
+    isInvincible = true;
+
+    for (float i = 0; i < iFrameDuration; i += iFrameDeltaTime)
+    {
+        yield return new WaitForSeconds(iFrameDeltaTime);
+    }
+
+    Debug.Log("Player is no longer invincible!");
+    isInvincible = false;
     }
 }
